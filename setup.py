3cbead9681617b63747e0258fef15d3e161ca9ee
Mathias Monnerville <hg@monnerville.com>
2010-12-17 18:23:51 +0100

    Changed development status to stable

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = (0, 5)
+VERSION = (0, 5, 1)
 
 # Dynamically calculate the version based on VERSION tuple
 if len(VERSION)>2 and VERSION[2] is not None:
@@ -20,13 +20,14 @@ setup(
 	url = 'https://bitbucket.org/matm/django-colissimo',
     license = 'GPL 3',
     platforms = ['any'],
-    classifiers = ['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-				   'License :: OSI Approved :: GNU General Public License (GPL)',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Framework :: Django'],
+    classifiers = [
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Framework :: Django'],
     packages = find_packages(),
     setup_requires=["setuptools_hg"],
     include_package_data = True,
